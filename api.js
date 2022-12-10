d1af4a4f5772900188ea07a42bd44837fcbdbdc1
Unclemortuary <artem_mihalych@mail.ru>
2022-12-10 23:49:41 +0300

    prototype of api module

--- api.js
+++ api.js
@@ -1,1 +1,35 @@
-const BASE_URL = 'http://62.84.124.35:5051';
+const BASE_URL = 'http://62.84.124.35';
+const BASE_PORT = 5051;
+const FILTER_URL_PREFIX = 'api/v1/dashboard_filter';
+
+export const getClientApiId = (responseHandler, params) => new Promise((resolve, reject) => fetch(`${BASE_URL}:${BASE_PORT}/${FILTER_URL_PREFIX}/client`, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: toClientRequestJSON(params)
+}).then(response => response.json().then(data => responseHandler(data.attribute_value)))); 
+
+export const getCategories = (responseHandler, params) => fetch(`${BASE_URL}:${BASE_PORT}/${FILTER_URL_PREFIX}/categories`, {
+    body: toCategoriesRequestJSON(params)
+}).then(response => responseHandler(response));
+
+export const getBrands = (responseHandler, params) => fetch(`${BASE_URL}:${BASE_PORT}/${FILTER_URL_PREFIX}/brand`, {
+    body: toBrandsRequestJSON(params)
+}).then(response => responseHandler(response));
+
+export const getProducts = (responseHandler, params) => fetch(`${BASE_URL}:${BASE_PORT}/${FILTER_URL_PREFIX}/products`, {
+    body: toProductsRequestJSON(params)
+}).then(response => responseHandler(response));
+
+
+
+const toClientRequestJSON = clientId => JSON.stringify({ client_id: clientId });
+const toCategoriesRequestJSON = ({ apiIdList, dateFrom, dateTo }) => JSON.stringify({ 
+    api_id_list: [apiIdList], date_from: dateFrom, date_to: dateTo });
+const toBrandsRequestJSON = ({ apiIdList, categoryIdList, dateFrom, dateTo }) => JSON.stringify({ 
+    api_id_list: [apiIdList], category_name_list: [categoryIdList], date_from: dateFrom, date_to: dateTo });
+const toProductsRequestJSON = ({ apiIdList, categoryIdList, brandNameList, dateFrom, dateTo }) => JSON.stringify({ 
+    api_id_list: [apiIdList], category_name_list: [categoryIdList], brand_name_list: [brandNameList], date_from: dateFrom, date_to: dateTo });
+
+// 155597
--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { createRoot } from 'react-dom/client';
 
+import * as api from './api';
+
 const App = () => {
+    const [clientId, setClientId] = useState(0);
+    const [categories, setCategories] = useState();
+    const [brands, setBrands] = useState();
+
     useEffect(() => {
-        initApp();
+        initApp({ setClientId, setCategories, setBrands });
     }, []);
 
     return <p>Hello</p>
 };
 
-const initApp = () => {
-
+const initApp = ({ setClientId, setCategories, setBrands }) => {
+    const CLIENT_ID = 32;
+    const handler = (data) => console.log(data);
+    const clientApiId = api.getClientApiId(handler, CLIENT_ID);
 };
 
 const root = createRoot(document.getElementById("root"));
